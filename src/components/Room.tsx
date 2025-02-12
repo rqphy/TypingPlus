abc00642ca8849a85ddfc62e930e8ed3c71e548d
rqphy <raphael.ferreira@hetic.net>
2025-02-12 21:49:29 +0100

    :sparkles: tweek iframe position

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -3,35 +3,32 @@ import { Html, useTexture } from "@react-three/drei"
 import { useControls } from "leva"
 
 export default function Room() {
-    const { nodes } = useGLTF("/models/room/room.glb")
+	const { nodes } = useGLTF("/models/room/room.glb")
 
-    const bakedTexture = useTexture("/models/room/baked.jpg")
-    bakedTexture.flipY = false
+	const bakedTexture = useTexture("/models/room/baked.jpg")
+	bakedTexture.flipY = false
 
-    // const { positionX, positionY, positionZ } = useControls({
-    //     positionX: { value: 0, min: -10, max: 10, step: 0.1 },
-    //     positionY: { value: 0, min: -10, max: 10, step: 0.1 },
-    //     positionZ: { value: 0, min: -10, max: 10, step: 0.1 },
-    // })
+	// const { positionX, positionY, positionZ } = useControls({
+	// 	positionX: { value: 0, min: -10, max: 10, step: 0.01 },
+	// 	positionY: { value: 0, min: -10, max: 10, step: 0.01 },
+	// 	positionZ: { value: 0, min: -10, max: 10, step: 0.01 },
+	// })
 
-    return (
-        <mesh
-            geometry={nodes.baked.geometry}
-            position={[0, -1, -0.8]}
-            // scale={0.3}
-        >
-            <meshBasicMaterial map={bakedTexture} />
-            <Html
-                transform
-                wrapperClass="htmlScreen"
-                distanceFactor={0.65}
-                position={[0, 2.693, -3.5]}
-            >
-                <iframe src="https://bruno-simon.com/" />
-            </Html>
-        </mesh>
-        // <primitive position={[0, -2.5, 2.6]} object={scene}>
+	return (
+		<mesh geometry={nodes.baked.geometry} position={[0, -1, -0.8]}>
+			<meshBasicMaterial map={bakedTexture} />
+			<Html
+				transform
+				wrapperClass="htmlScreen"
+				distanceFactor={0.65}
+				position={[0, 1.09, -0.53]}
+				// position={[positionX, positionY, positionZ]}
+			>
+				<iframe src="https://bruno-simon.com/" />
+			</Html>
+		</mesh>
+		// <primitive position={[0, -2.5, 2.6]} object={scene}>
 
-        // </primitive>
-    )
+		// </primitive>
+	)
 }
